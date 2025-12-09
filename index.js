c6d8b50fd75effba0507e5e753c0b667369ecaa1
MD Fahad Molla <ahfahad47sss@gmail.com>
2025-12-09 22:37:10 +0600

    get single serive and bookings added

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ async function run() {
     const db = client.db("style_decor_db");
     const userCollection = db.collection("users");
     const serviceCollection = db.collection("services");
+    const bookingCollection = db.collection("bookings");
     // add users
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -76,9 +77,18 @@ async function run() {
       const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
+    // change account status
+    app.patch("/users/status/:id", async (req, res) => {
+      const id = req.params.id;
+      const { status } = req.body; // 'active' or 'disabled'
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = { $set: { status: status } };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     // get user role
-    app.get("/users/:email/role", async (req, res) => {
+    app.get("/users/role/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email };
       const user = await userCollection.findOne(query);
@@ -90,9 +100,27 @@ async function run() {
       const result = await serviceCollection.insertOne(service);
       res.send(result);
     });
-    // get services
+    // get services and search filter
     app.get("/services", async (req, res) => {
-      const result = await serviceCollection.find().toArray();
+      const { search, category, min, max } = req.query;
+      let query = {};
+
+      if (search) {
+        query.service_name = { $regex: search, $options: "i" };
+      }
+
+      if (category) {
+        query.category = category;
+      }
+
+      // price
+      if (min || max) {
+        query.price = {};
+        if (min) query.price.$gte = parseFloat(min);
+        if (max) query.price.$lte = parseFloat(max);
+      }
+
+      const result = await serviceCollection.find(query).toArray();
       res.send(result);
     });
     // delete services
@@ -119,6 +147,45 @@ async function run() {
         },
       };
       const result = await serviceCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // get single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ error: "Invalid ID format" });
+      }
+
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+
+      if (
+        !booking.service_id ||
+        !booking.email ||
+        !booking.date ||
+        !booking.address
+      ) {
+        return res.status(400).send({ message: "Missing required fields" });
+      }
+
+      const query = {
+        email: booking.email,
+        service_id: booking.service_id,
+        date: booking.date,
+      };
+      const existingBooking = await bookingCollection.findOne(query);
+      if (existingBooking) {
+        return res.send({ message: "already booked", insertedId: null });
+      }
+
+      const result = await bookingCollection.insertOne(booking);
       res.send(result);
     });
 
